495611a0cdce4f49ee096903dd77b4cc5f1b7a14
Peng-Shan <pengshan27@gmail.com>
2023-03-30 12:57:11 -0700

    mode modification

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,6 +2,7 @@ import {
   HashRouter as Router,
   Route
 } from "react-router-dom";
+import React, { useState } from 'react';
 
 
 import './App.css';
@@ -11,11 +12,12 @@ import NotePage from './pages/NotePage'
 import LoginPage from "./pages/LoginPage";
 
 function App() {
+  const [mode, setMode] = useState('dark');
   return (
     <Router>
-      <div className="container dark">
+      <div className={`container ${mode}`}>
         <div className="app">
-          <Header />
+        <Header mode={mode} setMode={setMode} />
           <Route path='/' exact component={LoginPage} />
           <Route path="/home" exact component={NotesListPage} />
           <Route path="/note/:id" component={NotePage} />
--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,12 +1,18 @@
 import React from 'react'
 
-const Header = () => {
-
+const Header = ({ mode, setMode }) => {
+    const handleClick = () => {
+      setMode(mode === 'dark' ? 'light' : 'dark');
+    };
     return (
-        <div className="app-header">
-            <h1>WebNotes</h1>
-        </div>
-    )
-}
+      <div className="app-header">
+        <h1>WebNotes</h1>
+        <button onClick={handleClick}>
+          {mode === 'dark' ? 'Light Mode' : 'Dark Mode'}
+        </button>
+      </div>
+    );
+  };
 
 export default Header
+
